7ffa775c5a1a7fd763e79421c63e4db9cc630ac0
seon54 <seonhyeongpark54@gmail.com>
2019-10-22 15:13:15 +0900

    update all_rooms view with redirect

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,5 @@
-from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from rooms.models import Room
 
 
@@ -8,5 +7,8 @@ def all_rooms(request):
     page = request.GET.get('page', 1)
     room_list = Room.objects.all()
     paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    return render(request, 'rooms/home.html', context={'rooms': rooms})
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, 'rooms/home.html', context={'rooms': rooms})
+    except EmptyPage:
+        return redirect('/')
