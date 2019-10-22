0116bfbf03761942ed74fa504638200bc44a66ba
seon54 <seonhyeongpark54@gmail.com>
2019-10-22 14:56:18 +0900

    add Paginator

--- rooms/admin.py
+++ rooms/admin.py
@@ -23,7 +23,7 @@ class RoomAdmin(admin.ModelAdmin):
     fieldsets = (
         (
             "Basic Info",
-            {"fields": ("name", "description", "country", "address", "price")}
+            {"fields": ("name", "description", "country", "city", "address", "price")}
         ),
         (
             "Times",
