2aa3e7ff4dbfe6f048e96985e8e7a2842ae4233a
seon54 <seonhyeongpark54@gmail.com>
2019-10-23 16:04:29 +0900

    add search view

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,10 @@
 from django.http import Http404
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
 from django.urls import reverse
-from rooms.models import Room
+from django_countries import countries
+from rooms.models import Room, RoomType
 
 
 class HomeView(ListView):
@@ -19,3 +21,20 @@ class RoomDetail(DetailView):
     """ Room Detail Definition"""
 
     model = Room
+
+
+def search(request):
+    city = request.GET.get("city", "anywhere")
+    city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+    room_types = RoomType.objects.all()
+
+    form = {
+        "city": city, "s_room_type": room_type, "s_country": country
+    }
+
+    choices = {
+        "countries": countries, "room_types": room_types
+    }
+    return render(request, "rooms/search.html", {**form, **choices})
