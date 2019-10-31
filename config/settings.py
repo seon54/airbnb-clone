82ed2d2458e1d4d756fe184b339f416a6a980525
seon54 <seonhyeongpark54@gmail.com>
2019-10-31 13:53:54 +0900

    add django-dotenv & mailgun configuration

--- config/settings.py
+++ config/settings.py
@@ -132,3 +132,10 @@ AUTH_USER_MODEL = "users.User"
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
+
+
+# Email Configuration
+EMAIL_HOST = "smtp.mailgun.org"
+EMAIL_PORT = "587"
+EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
+EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
