2bba373fcd0a12cecacdefe7a10b5f73dd609b98
seon54 <seonhyeongpark54@gmail.com>
2019-11-06 14:44:00 +0900

    update SignUpForm with UserCreationForm

--- users/views.py
+++ users/views.py
@@ -32,11 +32,6 @@ class SignUpView(FormView):
     template_name = 'users/signup.html'
     form_class = SignUpForm
     success_url = reverse_lazy('core:home')
-    initial = {
-        'first_name': 'test2',
-        'last_name': 'test2',
-        'email': 'test2@email.com',
-    }
 
     def form_valid(self, form):
         form.save()
