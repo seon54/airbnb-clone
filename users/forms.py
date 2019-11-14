755aa7c798d79d5606fde79fd80f57bfc99aa157
seon54 <seonhyeongpark54@gmail.com>
2019-11-14 19:15:20 +0900

    update templates

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,10 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 
 class LoginForm(forms.Form):
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "Email"}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "Password"}))
 
     def clean(self):
         email = self.cleaned_data.get('email')
@@ -20,5 +19,31 @@ class LoginForm(forms.Form):
             self.add_error('email', forms.ValidationError("User does not exist."))
 
 
-class SignUpForm(UserCreationForm):
-    username = forms.EmailField(label='Email')
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = {"first_name", "last_name", "email"}
+        widgets = {
+            "first_name": forms.TextInput(attrs={"placeholder": "First Name"}),
+            "last_name": forms.TextInput(attrs={"placeholder": "Last Name"}),
+            "email": forms.TextInput(attrs={"placeholder": "Email"}),
+        }
+
+    password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "Password"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"}))
+
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        password1 = self.cleaned_data.get('password1')
+        if password != password1:
+            raise forms.ValidationError("Password confirmation does not match")
+        else:
+            return password
+
+    def save(self, *args, **kwargs):
+        user = super().save(commit=False)
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password')
+        user.username = email
+        user.set_password(password)
+        user.save()
