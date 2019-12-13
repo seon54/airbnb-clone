0307cde4f9983067d0dd6c73f4b9db7d329b0625
seon54 <seonhyeongpark54@gmail.com>
2019-12-13 17:36:00 +0900

    Update room detail

--- config/settings.py
+++ config/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = "ko-kr"
+LANGUAGE_CODE = "en-EN"
 
 TIME_ZONE = "Asia/Seoul"
 
--- users/admin.py
+++ users/admin.py
@@ -7,8 +7,8 @@ from users.models import User
 class CustomUserAdmin(UserAdmin):
     """Custom User Admin"""
     list_display = (
-    'username', 'first_name', 'last_name', 'email', 'is_active', 'language', 'currency', 'superhost',
-    'is_staff', 'is_superuser', 'email_verified', 'email_secret', 'login_method')
+        'username', 'first_name', 'last_name', 'email', 'is_active', 'language', 'currency', 'superhost',
+        'is_staff', 'is_superuser', 'email_verified', 'email_secret', 'login_method')
     list_filter = UserAdmin.list_filter + ('superhost',)
     fieldsets = UserAdmin.fieldsets + (
         (
